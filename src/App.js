8f029e37caae7f6c91f754b1cb217f3af11c7021
cosmic-boy0111 <gauravbhagat2040@gmail.com>
2024-09-14 13:25:39 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
+// Import images from assets folder
+import image1 from './assets/mask2.jpg';
+import image2 from './assets/profile.jpg';
+
 function App() {
+  const [droppedImage, setDroppedImage] = useState(null);
+  const [images] = useState([
+    { src: image1, alt: 'Image 1' },
+    { src: image2, alt: 'Image 2' },
+    { src: image1, alt: 'Image 2' },
+    { src: image2, alt: 'Image 2' },
+    { src: image1, alt: 'Image 2' },
+  ]);
+  const dropZoneRef = useRef(null);
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const droppedFile = e.dataTransfer.files[0];
+    if (droppedFile.type.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.onload = (e) => setDroppedImage(e.target.result);
+      reader.readAsDataURL(droppedFile);
+    }
+  };
+
+  // const handleImageClick = (imageSrc) => {
+  //   setDroppedImage(imageSrc);
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div 
+        className="drop-zone" 
+        ref={dropZoneRef}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        {droppedImage && <img src={droppedImage} alt="Dropped" className="dropped-image" />}
+      </div>
+      <div className="image-list">
+        {images.map((image, index) => (
+          <img 
+            key={index} 
+            src={image.src} 
+            alt={image.alt} 
+            // onClick={() => handleImageClick(image.src)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
